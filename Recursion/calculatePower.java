2007c3c5b5e7ce9fac1b0cbd640b5938c6314847
Samya Thakur <thakursamya@gmail.com>
2023-06-29 21:24:43 +0530

    calculatePower.java

---
+++ Recursion/calculatePower.java
@@ -0,0 +1,56 @@
+/*
+Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+Do this recursively.
+Input format :
+Two integers x and n (separated by space)
+Output Format :
+x^n (i.e. x raise to the power n)
+Constraints :
+0 <= x <= 30
+0 <= n <= 30
+Sample Input 1 :
+ 3 4
+Sample Output 1 :
+81
+Sample Input 2 :
+ 2 5
+Sample Output 2 :
+32 
+2 2 2 2 2
+ */
+public class calculatePower {
+	
+	public static int power(int x, int n) {
+		 if(x == 0 && n == 0 ) {
+				return 1;
+			}
+			else if(x == 0 && n !=0){
+				return 0;
+			}
+			else if(x != 0 && n ==0) {
+				return 1;
+			}
+				
+			else if(x != 0 && n == 1 ) {
+				return x;
+			}
+		
+		
+		
+		int smallerValue = power(x, n-1 );
+		
+		int result = x * smallerValue;
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.print("what is the value of x ? ");
+		int x = s.nextInt();
+		System.out.print("what is the value of n ? ");
+		int n = s.nextInt();
+		System.out.print(power(x,n));
+	}
+
+}
