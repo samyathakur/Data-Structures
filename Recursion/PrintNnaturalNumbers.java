8c0ed27969b23072aeb4698f6be57153f91c90df
Samya Thakur <thakursamya@gmail.com>
2023-06-29 14:41:48 +0530

    PrintNnaturalNumbers.java

---
+++ Recursion/PrintNnaturalNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class printNaturalNumber {
+	
+	public static void printnumber(int n) {
+		
+		if(n==0) {
+			return;
+		}
+		printnumber(n-1);
+		System.out.println(n);
+	}
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		printnumber(n);
+	}
+}
